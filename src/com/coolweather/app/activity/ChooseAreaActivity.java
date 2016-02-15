81735b7e566d4e7cc6d9b369f95f4eb3464caeff
Jim <360677148@qq.com>
2016-02-15 21:28:22 +0800

    新增手动切换城市和手动更新天气的功功能

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -46,11 +49,23 @@ public class ChooseAreaActivity extends Activity
 	private City selectedCity;
 	private int currentLevel;
 	
+	private boolean isFromWeatherActivity;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity",false);
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(prefs.getBoolean("city_selected",false) && !isFromWeatherActivity)
+		{
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+			
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView)findViewById(R.id.list_view);
@@ -74,7 +89,14 @@ public class ChooseAreaActivity extends Activity
 					selectedCity = cityList.get(position);
 					queryCounties();
 				}
-				
+				else if(currentLevel == LEVEL_COUNTY)
+				{
+					String countyCode = countyList.get(position).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code",countyCode);
+					startActivity(intent);
+					finish();
+				}
 			}
 			
 		});
@@ -269,6 +291,12 @@ public class ChooseAreaActivity extends Activity
 		}
 		else 
 		{
+			if(isFromWeatherActivity)
+			{
+				Intent intent = new Intent(this,WeatherActivity.class);
+				startActivity(intent);
+				
+			}
 			finish();
 		}
 		
